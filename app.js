03b4c25f3bb06aaa84f32a4cd14a6e6088110062
Adnan Khan <adnankhan48978@gmail.com>
2026-04-10 12:13:26 +0500

    second commite

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.write("Hello from Node.js App 🚀");
+  res.write("Hello from Node.js App again deploy🚀");
   res.end();
 });
-
+//again deploy
 server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
